a6feb2e327f23de3055983e7109d5ab205e39699
Ehsan Davari <70313192+iamehsandvr@users.noreply.github.com>
2022-02-01 15:41:12 +0330

    Update ProProfile.plugin.js

--- ProProfile.plugin.js
+++ ProProfile.plugin.js
@@ -2,7 +2,7 @@
  * @name ProProfile
  * @author EhsanDavari
  * @authorId 553139953597677568
- * @version 1.0.1
+ * @version 1.0.2
  * @description  with this plugin : You can copy the user banner (banner color and banner photo) You can copy the user's profile picture You can copy About Me to the user You can also copy the user bio
  * @invite xfvHwqXXKs
  * @website https://www.beheshtmarket.com
@@ -22,13 +22,17 @@ const config = {
                 name: "EhsanDavari"
             }
         ],
-        version: "1.0.1",
+        version: "1.0.2",
         description: " with this plugin : You can copy the user banner (banner color and banner photo) You can copy the user's profile picture You can copy About Me to the user You can also copy the user bio",
         changelog: [
             {
-                title: "Bugs Squashed",
+                title: 'Cool Update',
+                type: 'improved',
                 items: [
-                    "Clicking on the user's profile picture in the member list would save the avatar."
+                    "Double-click to copy the server profile",
+                    "Double-click to copy the server banner ",
+                    "برای کپی کردن پروفایل سرور دوبار کلیک کنید",
+                    "برای کپی کردن بنر سرور دوبار کلیک کنید",
                 ]
             },
         ]
@@ -78,6 +82,19 @@ module.exports = !global.ZeresPluginLibrary ? class {
             const UserBio = WebpackModules.find(m => m?.default?.displayName === "UserBio");
             const UserBanner = WebpackModules.find(m => m?.default?.displayName === "UserBanner");
             const CustomStatus = WebpackModules.find(m => m?.default?.displayName === "CustomStatus");
+            document.addEventListener("dblclick", ({ target }) => {
+                if (new RegExp(/guild*/).test(target.dataset.listItemId)) {
+                    global.ZLibrary.DiscordModules.ElectronModule.copy(target.children[0].currentSrc.replace(/([0-9]+)$/, "4096"));
+                    return BdApi.showToast(`Server icon link successfully copied`, { type: "success" });
+                } else if (new RegExp(/animatedBannerHoverLayer*/).test(target.className)) {
+                    global.ZLibrary.DiscordModules.ElectronModule.copy(`https://cdn.discordapp.com/banners/${target.__reactFiber$.return.memoizedProps.guild.id}/${target.__reactFiber$.return.memoizedProps.guildBanner}.gif?size=4096`);
+                    return BdApi.showToast(`Server banner link successfully copied`, { type: "success" });
+                } else if (new RegExp(/height:*/).test(target.style.cssText)) {
+                    var strGuildBanner = document.getElementsByClassName("animatedContainer-2laTjx");
+                    global.ZLibrary.DiscordModules.ElectronModule.copy(strGuildBanner[0].children[0].children[0].currentSrc.replace(/([0-9]+)$/, "4096"));
+                    return BdApi.showToast(`Server icon link successfully copied`, { type: "success" });
+                }
+            });
             document.addEventListener("click", ({ target }) => {
                 if (target.ariaLabel && target.style.cssText && (new RegExp(/avatar\-3QF_VA/).test(target.className))) {
                     let MemberProfileUrl = target.__reactProps$.children.props.children[0].props.children.props.src;
